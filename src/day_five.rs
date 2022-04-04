81331e26e0805c244e15a8c59907951978870853
Grant Horner <h.grant.horner@gmail.com>
2022-04-04 18:54:46 -0400

    finished d5p1

--- src/day_five.rs
+++ src/day_five.rs
@@ -1,40 +1,14 @@
 #![warn(unused_variables, dead_code)]
 
-use std::convert::TryFrom;
-use std::convert::TryInto;
-
 trait GetPlace {
     fn get_place(self, place: Place) -> usize;
-}
-
-#[derive(Debug)]
-pub struct Operation<'a> {
-    code: OpCode,
-    count: u8,
-    parameters: &'a [Parameter],
-}
-
-impl<'a> Operation<'_> {
-    fn new(code: OpCode, count: u8, parameters: &[Parameter]) -> Operation {
-        Operation {
-            code,
-            count,
-            parameters,
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct Parameter {
-    mode: Mode,
-    value: i32,
 }
 
 #[derive(PartialEq, Debug)]
 pub enum OpCode {
     Add = 1,
     Mul = 2,
-    Save = 3,
+    Input = 3,
     Out = 4,
     Halt = 99,
 }
@@ -45,56 +19,80 @@ pub enum Mode {
     Immediate = 1,
 }
 
-impl TryFrom<usize> for OpCode {
-    type Error = ();
-
-    fn try_from(value: usize) -> Result<Self, Self::Error> {
+impl From<usize> for OpCode {
+    fn from(value: usize) -> Self {
         match value % 100 {
-            x if x == OpCode::Add as usize => Ok(OpCode::Add),
-            x if x == OpCode::Mul as usize => Ok(OpCode::Mul),
-            x if x == OpCode::Save as usize => Ok(OpCode::Save),
-            x if x == OpCode::Out as usize => Ok(OpCode::Out),
-            x if x == OpCode::Halt as usize => Ok(OpCode::Halt),
-            _ => Err(()),
+            x if x == OpCode::Add as usize => OpCode::Add,
+            x if x == OpCode::Mul as usize => OpCode::Mul,
+            x if x == OpCode::Input as usize => OpCode::Input,
+            x if x == OpCode::Out as usize => OpCode::Out,
+            x if x == OpCode::Halt as usize => OpCode::Halt,
+            _ => panic!("Should have received a valid OpCode, got {}", value),
         }
     }
 }
 
-pub fn perform_ops(ops: &mut Vec<usize>) {
+impl From<usize> for Mode {
+    fn from(value: usize) -> Self {
+        match value {
+            x if x == Mode::Immediate as usize => Mode::Immediate,
+            x if x == Mode::Position as usize => Mode::Position,
+            _ => panic!("Should have only received 0 or 1! Got {}", value),
+        }
+    }
+}
+
+fn parse_op(x: usize) -> (Mode, Mode, Mode, OpCode) {
+    (
+        x.get_place(Place::TenThousand).into(),
+        x.get_place(Place::Thousand).into(),
+        x.get_place(Place::Hundred).into(),
+        OpCode::from(x),
+    )
+}
+
+fn get_val(ops: &Vec<i64>, index: usize, mode: Mode) -> i64 {
+    match mode {
+        Mode::Position => ops[ops[index] as usize],
+        Mode::Immediate => ops[index],
+    }
+}
+
+pub fn perform_ops(ops: &mut Vec<i64>) {
     let mut index = 0;
-    let mut op: OpCode = ops[index]
-        .try_into()
-        .expect(format!("Failed to unwrap {:?}", ops[index]).as_str());
+    let (mut _mode3, mut mode2, mut mode1, mut op) = parse_op(ops[index] as usize);
 
     while op != OpCode::Halt {
         match op {
             OpCode::Add => {
-                let arg1 = ops[ops[index + 1]];
-                let arg2 = ops[ops[index + 2]];
-                let target_index = ops[index + 3];
+                let arg1 = get_val(&ops, index + 1, mode1);
+                let arg2 = get_val(&ops, index + 2, mode2);
+                let target_index = ops[index + 3] as usize;
                 ops[target_index] = arg1 + arg2;
                 index += 4;
             }
             OpCode::Mul => {
-                let arg1 = ops[ops[index + 1]];
-                let arg2 = ops[ops[index + 2]];
-                let target_index = ops[index + 3];
+                let arg1 = get_val(&ops, index + 1, mode1);
+                let arg2 = get_val(&ops, index + 2, mode2);
+                let target_index = ops[index + 3] as usize;
                 ops[target_index] = arg1 * arg2;
                 index += 4;
             }
-            OpCode::Save => {
+            OpCode::Input => {
                 let arg = ops[index + 1];
-                ops[arg] = arg;
+                // Hardcoded input value
+                ops[arg as usize] = 1;
                 index += 2;
             }
             OpCode::Out => {
-                let arg = ops[index + 1];
+                let arg = ops[ops[index + 1] as usize];
+                println!("Out: {}", arg);
                 index += 2;
             }
             _ => panic!("Invalid opcode {:?} at index {:?}", ops[index], index),
         }
 
-        op = ops[index].try_into().unwrap();
+        (_mode3, mode2, mode1, op) = parse_op(ops[index] as usize);
     }
 }
 
@@ -144,7 +142,13 @@ mod tests {
 
     #[test]
     fn scratch() {
-        let x = 12345;
-        println!("{}", (x % 100) / 10)
+        let path = std::path::Path::new("resources/day_five_input.txt");
+        let mut input: Vec<i64> = std::fs::read_to_string(path)
+            .unwrap()
+            .split(",")
+            .map(|x| x.parse().unwrap())
+            .collect();
+
+        perform_ops(&mut input);
     }
 }
